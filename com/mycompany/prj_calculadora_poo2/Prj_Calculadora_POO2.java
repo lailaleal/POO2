f1285ec2e359c5fbed130e097e871c87e834ac3f
Laila Leal <135389321+lailaleal@users.noreply.github.com>
2025-04-28 19:47:11 -0300

    Add files via upload

---
+++ com/mycompany/prj_calculadora_poo2/Prj_Calculadora_POO2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prj_calculadora_poo2;
+
+/**
+ *
+ * @author laila
+ */
+public class Prj_Calculadora_POO2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
